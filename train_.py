e1122d244015f890a28e8640bd96f82e9bafc31b
dragoon1251996 <whiyew@gmail.com>
2018-10-30 10:36:03 +0700

    Add files via upload

--- train_.py
+++ train_.py
@@ -15,7 +15,7 @@ def put(name_database,name_table):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="okmijnuhb",
+        passwd="",
         database=name_database
     )
     mycursor = mydb.cursor()
@@ -34,7 +34,8 @@ def put(name_database,name_table):
     es = Elasticsearch([{'host': 'localhost', 'post': '9200'}])
     for i in range(len(out)):
         es.index(index=name_database, doc_type='qa', id=int(out[i]["id"]), body=out[i])
-
+    a=es.search(index=name_database, body={"query": {"match": {'question':'ai cho em'}}})
+    print(a)
 
 if __name__ == '__main__':
     put(sys.argv[1],sys.argv[2])
