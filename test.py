26f8b0f728702e323b478e47dcca2eb9c93bf702
dragoon1251996 <whiyew@gmail.com>
2018-10-30 10:26:53 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from elasticsearch import Elasticsearch
+
+def put(name_database,name_table):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="okmijnuhb",
+        database=name_database
+    )
+    mycursor = mydb.cursor()
+    sql="SELECT * from "+name_table
+    mycursor.execute(sql)
+    data=mycursor.fetchall()
+    print(data)
+    es = Elasticsearch([{'host': 'localhost', 'post': '9200'}])
+    a=es.search(index="bot1", body={"query": {"match": {'question':'ai cho em'}}})
+    print(a)
+
+if __name__ == '__main__':
+    put("b1","qa")
