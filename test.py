97ad7a9353d37b79ca25f2150ab855f21791bb17
dragoon1251996 <whiyew@gmail.com>
2018-10-31 09:46:33 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -13,9 +13,10 @@ def put(name_database,name_table):
     mycursor.execute(sql)
     data=mycursor.fetchall()
     print(data)
-    es = Elasticsearch([{'host': 'localhost', 'post': '9200'}])
-    a=es.search(index="b1", body={"query": {"match": {'question':'ai cho em'}}})
-    print(a)
+    # es = Elasticsearch([{'host': 'localhost', 'post': '9200'}])
+    # a=es.search(index="b1", body={"query": {"match": {'question':'ai cho em'}}})
+    # print(a)
 
 if __name__ == '__main__':
     put("b1","qa")
+    put("bot1","qa")
