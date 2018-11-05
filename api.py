15b884d8af306ea7ad8cece113c148cce903027d
dragoon1251996 <whiyew@gmail.com>
2018-11-06 06:09:02 +0700

    Add files via upload

--- api.py
+++ api.py
@@ -27,7 +27,7 @@ class Post(Resource):
             test = dict(request.form)[chatbot_name][0]
             return {"answer": [x["_source"]["answer"] for x in es.search(index=chatbot_name, body={"query": {"match": {'question': test}}})["hits"]["hits"]]}
         except Exception as e:
-            return str(e)+chatbot_name +str(test)
+            return str(e)+chatbot_name +str(request.form)
 api.add_resource(Post,'/QA')
 
 if __name__ =="__main__":
